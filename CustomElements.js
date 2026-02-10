e5fe6b10e4ff11eedbcc9626f0701155e7b0b7c8
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-02-10 19:35:50 +1100

    Update CustomElements.js

--- CustomElements.js
+++ CustomElements.js
@@ -3,6 +3,17 @@ console.log("working just fine!")
 //OOP - Inheritence
 class MyHeader extends HTMLElement {
   conectedCallback() {
-      this.innerHTML = ``
+      this.innerHTML = `
+      <header>
+     <h1>The Resume of ME :D</h1>
+      <div id="navigationButtons">
+        <button onclick="window.location.href = `index.html`">Home</button>
+        <button onclick="window.location.href = `HTMLGames.html`">HTML Games</button>
+        <button onclick="window.location.href = `game1.html`">The Bridge</button>
+        <button onclick="window.location.href = `game2.html`">The Pizza Ria</button>
+        <button onclick="window.location.href = `game3.html`">Game #3</button>
+      </div>
+    </header>
+    `
   }
 }

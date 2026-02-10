954637b2768dc3bec49050dc15017434fe348586
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-02-10 19:41:38 +1100

    Update CustomElements.js

--- CustomElements.js
+++ CustomElements.js
@@ -4,16 +4,14 @@ console.log("working just fine!")
 class MyHeader extends HTMLElement {
   connectedCallback() {
       this.innerHTML = `
-      <header>
      <h1>The Resume of ME :D</h1>
       <div id="navigationButtons">
-        <button onclick="window.location.href = `index.html`">Home</button>
-        <button onclick="window.location.href = `HTMLGames.html`">HTML Games</button>
-        <button onclick="window.location.href = `game1.html`">The Bridge</button>
-        <button onclick="window.location.href = `game2.html`">The Pizza Ria</button>
-        <button onclick="window.location.href = `game3.html`">Game #3</button>
+        <button onclick="window.location.href = 'index.html'">Home</button>
+        <button onclick="window.location.href = 'HTMLGames.html'">HTML Games</button>
+        <button onclick="window.location.href = 'game1.html'">The Bridge</button>
+        <button onclick="window.location.href = 'game2.html'">The Pizza Ria</button>
+        <button onclick="window.location.href = 'game3.html'">Game #3</button>
       </div>
-    </header>
     `
   }
 }

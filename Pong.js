828d2009a2f5281e2384b3506be81996704546d7
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-02-17 19:28:31 +1100

    Update Pong.js

--- Pong.js
+++ Pong.js
@@ -3,6 +3,9 @@ console.log("Pong")
 let myCanvas;
 
 function setup() {
- myCanvas = 
+ myCanvas = createCanvas(500, 500);
  myCanvas.parent('gamePanel'); //fix
 }
+ function draw() {
+    background(255, 0, 255);
+ }

a37c23f0e95f4c3e3fd80443053ab5632ef12176
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-02-24 19:11:09 +1100

    Update HTMLGameManager.js

--- HTMLGameManager.js
+++ HTMLGameManager.js
@@ -1,1 +1,8 @@
+var loadedGame = document.createElement("script");
+loadedGame.src = "Games/Pong.js"
+document.head.appendChild(loadedGame);
 
+function loadNewGame( filename) {
+  sessionStorage.setItem("loadedGame", fileName)
+  location.reload()
+}

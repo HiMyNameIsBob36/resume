f5a6e86039c64e2e80953ec8c8368b977033678b
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-02-24 19:49:58 +1100

    Update openerCheck.js

--- openerCheck.js
+++ openerCheck.js
@@ -1,5 +1,6 @@
 var openerElement = document.getElementById("opener")
-sessionStorage.setItem("visitorName", "Micky Mouse")
+// sessionStorage.setItem("visitorName", "Micky Mouse");
+// sessionStorage.removeItem("visitorName");
 
 if (sessionStorage.getItem("visitorName") == null
   openerElement style.display = "block";

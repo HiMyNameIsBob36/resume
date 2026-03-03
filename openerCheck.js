8b4ee0b0b6f78fc8113499c5c8bf853a381025ba
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-03 18:59:21 +1100

    Update openerCheck.js

--- openerCheck.js
+++ openerCheck.js
@@ -6,3 +6,13 @@ if (sessionStorage.getItem("visitorName") == null
   openerElement style.display = "block";
 else
   openerElement style.display = "none";
+
+function registerNewVisitor( anonCheck ) {
+  if (anonCheck) {
+    sessionStorage.setItem("visitorName", "anon")
+    sessionStorage.setItem("visitorCompany", "anon")
+  } else {
+    sessionStorage.setItem("visitorName", document.getElementById("visitorName").value)
+    sessionStorage.setItem("visitorCompany", document.getElementById("visitorCompany").value")
+  }
+}

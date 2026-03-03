1f75ae935f03d3fea9201e862494f3a9648c9d44
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-03 19:24:41 +1100

    Update openerCheck.js

--- openerCheck.js
+++ openerCheck.js
@@ -2,30 +2,14 @@ var openerElement;
 
 window.customElements.whenDefined('my-opener').then( function() { 
    var openerElement = document.getElementById("opener");
-})
 
-var openerElement = document.getElementById("opener")
-// sessionStorage.setItem("visitorName", "Micky Mouse");
-// TURN OFF WHEN READY TO POST
-sessionStorage.removeItem("visitorName");
-sessionStorage.removeItem("visitorCompany");
-
-if (sessionStorage.getItem("visitorName") == null
+   if (sessionStorage.getItem("visitorName") == null)
   openerElement style.display = "block";
-else
+   else
   openerElement style.display = "none";
 
-function registerNewVisitor( anonCheck ) {
-  if (anonCheck) {
-    sessionStorage.setItem("visitorName", "anon")
-    sessionStorage.setItem("visitorCompany", "anon")
-  } else {
-    sessionStorage.setItem("visitorName", document.getElementById("visitorName").value)
-    sessionStorage.setItem("visitorCompany", document.getElementById("visitorCompany").value)
-  }
-}
 
-document.getElementById("SubmitVisitor").addEventListener('click', function(event) {
+   document.getElementById("SubmitVisitor").addEventListener('click', function(event) {
   event.preventDefault();
   registerNewVisitor( false);
   openElement.style.display = "none"
@@ -36,3 +20,22 @@ document.getElementById("SubmitAnon").addEventListener('click', function(event) 
   registerNewVisitor( true );
   openElement.style.display = "none"
 })
+
+})
+
+var openerElement = document.getElementById("opener")
+// sessionStorage.setItem("visitorName", "Micky Mouse");
+// TURN OFF WHEN READY TO POST
+sessionStorage.removeItem("visitorName");
+sessionStorage.removeItem("visitorCompany");
+
+
+function registerNewVisitor( anonCheck ) {
+  if (anonCheck) {
+    sessionStorage.setItem("visitorName", "anon")
+    sessionStorage.setItem("visitorCompany", "anon")
+  } else {
+    sessionStorage.setItem("visitorName", document.getElementById("visitorName").value)
+    sessionStorage.setItem("visitorCompany", document.getElementById("visitorCompany").value)
+  }
+}
